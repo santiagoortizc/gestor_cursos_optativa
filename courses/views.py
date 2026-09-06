41bc95aba21c34d68c132fbc40cf432ca5d62230
Cristian <krcamilo0417@gmail.com>
2026-09-05 22:44:55 -0500

    listar lecciones

--- courses/views.py
+++ courses/views.py
@@ -19,7 +19,7 @@ def courses(request):
 
 def course_detail(request, course_id):
     course = Course.objects.get(id=course_id)
-    return render(request, "course_detail.html", {"course": course})
+    return render(request, "course_detail.html", {"course": course, "lessons": course.lessons_set.all()})
 
 
 def new_course(request):
